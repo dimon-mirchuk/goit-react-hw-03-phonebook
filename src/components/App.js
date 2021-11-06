c63ee0709c390e5adc84c4f13d92386f7f6a3775
dimon-mirchuk <dimon.mirchuk@gmail.com>
2021-11-06 16:50:09 +0300

    Task completed

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,24 @@ import { v4 as uuidv4 } from "uuid";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
+
+  componentDidMount() {
+    const getContacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(getContacts);
+    parsedContacts && this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem("contacts", JSON.stringify(nextContacts));
+    }
+  }
 
   handleAddContact = ({ name, number }) => {
     const contact = {
@@ -58,7 +68,7 @@ class App extends Component {
   };
 
   render() {
-    const { filter } = this.state;
+    const { contacts, filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
 
     return (
@@ -66,11 +76,17 @@ class App extends Component {
         <h1>Phonebook</h1>
         <ContactAddForm onSubmit={this.handleAddContact} />
         <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList
-          contacts={visibleContacts}
-          onDelete={this.handleDeleteContact}
-        />
+        {contacts.length > 1 && (
+          <Filter value={filter} onChange={this.changeFilter} />
+        )}
+        {contacts.length > 0 ? (
+          <ContactList
+            contacts={visibleContacts}
+            onDelete={this.handleDeleteContact}
+          />
+        ) : (
+          <p>Your phonebook is empty. Please add contact.</p>
+        )}
       </Container>
     );
   }
